e9d4cde449bb924f3973bdb0270e111ba8118641
Bagus Panji <47809071+Baguspanji@users.noreply.github.com>
2025-07-29 01:53:29 +0700

    feat: update application name and namespace to 'edukita'

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.educational_app"
+    namespace = "com.baguspanji.edukita"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.educational_app"
+        applicationId = "com.baguspanji.edukita"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
