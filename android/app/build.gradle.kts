136f143128c8d5a0868139478bd743fc54e768e6
Bagus Panji <47809071+Baguspanji@users.noreply.github.com>
2025-07-30 02:10:14 +0700

    fix: update NDK version and file_picker dependency to match plugin requirements

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.baguspanji.gastrofun"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // Fixed NDK version to match plugin requirements
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
